d696a321a4032f3c7d94874254d55be47455e902
yanjiawei <179811667@qq.com>
2026-04-10 02:35:30 +0800

    no message

--- docker-build/python3/pre_build.sh
+++ docker-build/python3/pre_build.sh
@@ -6,9 +6,11 @@ before_build() {
 
 pre_build() {
     local arch="$1"
-    cd ./${repo}
-    wget "http://public:Public123@192.168.60.100:8824/python3/Python-3.13.13.tgz"
-    tar xvf Python-3.13.13.tgz
-    mv Python-3.13.13 Python-3.13
+    local num="${tag#py}"
+    local python="Python-${num:0:1}.${num:1}"
+    cd "./${repo}"
+    echo "下载${python}.tar.gz"
+    wget "http://public:Public123@192.168.60.100:8824/python3/${python}.tar.gz"
+    tar xvf "${python}.tar.gz"
     cd ..
 }
