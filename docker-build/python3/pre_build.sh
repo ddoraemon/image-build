9d3a354689a9c31369819c407dd7e12b5756e3b2
Cloud <179811667@qq.com>
2026-06-25 11:21:52 +0800

    update

--- docker-build/python3/pre_build.sh
+++ docker-build/python3/pre_build.sh
@@ -10,6 +10,6 @@ pre_build() {
     local python="Python-${num:0:1}.${num:1}"
     cd "./${repo}"
     echo "下载${python}.tar.gz"
-    wget "http://public:Public123@10.0.0.215:8824/python3/${python}.tar.gz"
+    wget "http://public:Public123@${RESOURCE_IP}:8824/python3/${python}.tar.gz"
     cd ..
 }
