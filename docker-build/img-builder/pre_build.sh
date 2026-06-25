af49218aeb7f25ad1bb7a67203edb36c63520547
Cloud <179811667@qq.com>
2026-06-25 18:41:44 +0800

    update

--- docker-build/img-builder/pre_build.sh
+++ docker-build/img-builder/pre_build.sh
@@ -17,19 +17,4 @@ pre_build() {
     wget "http://public:Public123@${RESOURCE_IP}:8824/jdk/maven-3.19.2.tar.gz"
     tar xvf maven-3.19.2.tar.gz
     cd ..
-
-
-#    if [[ "$arch" == "arm64" ]]; then
-#        echo "👉 执行 arm64 的 pre_build"
-#        # arm64 专用代码
-#        do_something_for_arm64
-#    elif [[ "$arch" == "amd64" ]]; then
-#        echo "👉 执行 amd64 的 pre_build"
-#        # amd64 专用代码
-#        do_something_for_amd64
-#    else
-#        echo "❌ 不支持的架构: $arch"
-#        return 1
-#    fi
-
 }
