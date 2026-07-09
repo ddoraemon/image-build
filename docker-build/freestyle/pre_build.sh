c5fe3f1f84fa4b8f93d7d805adee7ab542abf2e5
yanjiawei <179811667@qq.com>
2026-07-09 23:40:44 +0800

    更新脚本

--- docker-build/freestyle/pre_build.sh
+++ docker-build/freestyle/pre_build.sh
@@ -9,8 +9,8 @@ pre_build() {
     echo "当前要执行${arch}的构建前操作"
     cd ./${repo}
     echo "下载mihomo-linux-${arch}-v${tag}"
-    wget -O freestyle "http://public:Public123@192.168.60.100:8824/clash/${arch}/mihomo-linux-${arch}-v${tag}"
-    wget "http://public:Public123@192.168.60.100:8824/clash/zashboard/dist.zip"
+    wget -O freestyle "http://public:Public123@${RESOURCE_IP}:8824/clash/${arch}/mihomo-linux-${arch}-v${tag}"
+    wget "http://public:Public123@${RESOURCE_IP}:8824/clash/zashboard/dist.zip"
     unzip dist.zip
     mv dist zashboard
     cd ..
